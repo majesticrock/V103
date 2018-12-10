75c11f33e3741640dd14eaafac4d5f3024e15811
majesticrock <joshua.althueser@web.de>
2018-12-10 06:26:37 +0100

    Durchführung Diskussion

--- ela.py
+++ ela.py
@@ -20,5 +20,24 @@ a = [ufloat(63.9, 0.5) * (10**(-3)), ufloat(66.2, 0.5) * (10**(-3)), ufloat(6.13
 #rechts, beidseitig
 #links, beidseitg
 
-for i in range(4):
-    print((m[i]*g)/(2*I[i]*a[i]))
+print((m[0]*g)/(2*I[0]*a[0]))
+print((m[1]*g)/(2*I[1]*a[1]))
+
+e1 = ((m[2]*g)/(48*I[2]*a[2]))
+e2 = ((m[3]*g)/(48*I[3]*a[3]))
+
+print(e1)
+print(e2)
+
+print((e1 + e2)/2)
+
+print("--------------------")
+
+l = [0.551, 0.6, 0.6]
+sm = [0.1671, 0.5024, 0.1213]
+
+print(sm[0]/(np.pi * (d[0]/2)**2) * l[0])
+
+print(sm[1]/(l[1] * d[1]**2))
+
+print(sm[2]/(l[2] * d[2]**2))
